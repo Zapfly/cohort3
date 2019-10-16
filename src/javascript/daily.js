5a490584e0cc5a8f9ae71052f809a93a0f15f8d3
Luke <seegerlucas295@gmail.com>
2019-10-16 13:37:41 -0600

    fixed daily formatting

--- src/javascript/daily.js
+++ src/javascript/daily.js
@@ -1,78 +1,3 @@
-/*	
-   Write the function that will create this output:
-
-*** the two values are not the same:
-   p1--> a
-   p2--> b
-*** the two values are not the same:
-   p1--> 1
-   p2--> 2
-*** the two values are not the same:
-   p1--> 2
-   p2--> 2
-*/
-
-// Write the function after this comment ---
-
-export const assertEquals = (p1, p2) => {
-    if (p1 === p2) return true
-    console.log(false)
-    /*console.log("*** the two values are not the same” and also log the two parameters as follows:");
-    console.log('“p1-->”' + p1);
-    console.log('“p2-->”' + p2);*/
-    return "the two values are not the same"
-}
-
-
-assertEquals("a", "b");
-assertEquals("a", "a");
-assertEquals(1, 2);
-assertEquals(2, 2);
-assertEquals("2", 2);
-assertEquals("This value", "This value");
-
-export const functions = {
-    makeEmailArr: (x) => {
-        return x[0].toLowerCase() + "." + x[1].toLowerCase() + "@evolveu.ca";
-    }
-}
-
-//OCT 15
-
-
-export const basics = {
-    xfor: () => {
-        let testArray = [1, 2, 5, 7, 5];
-        console.log(testArray)
-        let i = 0;
-        let emptyArray = [];
-        for (i = 0; i < testArray.length; i++) {
-            let x = testArray[i] + 2;
-            emptyArray.push(x);
-        }
-        console.log(testArray);
-        return emptyArray;
-    },
-    xwhile: () => {
-        let testArray = [1, 2, 5, 7, 5];
-        let i = 0;
-        while (i < 6) {
-            testArray.push(2);
-            i++
-        }
-        return testArray;
-    },
-    xdoWhile: () => {
-        let testArray = [1, 2, 5, 7, 5];
-        let i = 0;
-        do{
-            testArray.push(3);
-            i++
-        } while (i < 6);
-        return testArray;
-    },
-}
-
 //OCT 16
 
 let animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
@@ -120,3 +45,85 @@ export const farm = {
         return animals.sort();
     }
 }
+
+
+//SECOND DAILY
+
+
+export const functions = {
+    makeEmailArr: (x) => {
+        return x[0].toLowerCase() + "." + x[1].toLowerCase() + "@evolveu.ca";
+    }
+}
+
+//OCT 15
+
+
+export const basics = {
+    xfor: () => {
+        let testArray = [1, 2, 5, 7, 5];
+        console.log(testArray)
+        let i = 0;
+        let emptyArray = [];
+        for (i = 0; i < testArray.length; i++) {
+            let x = testArray[i] + 2;
+            emptyArray.push(x);
+        }
+        console.log(testArray);
+        return emptyArray;
+    },
+    xwhile: () => {
+        let testArray = [1, 2, 5, 7, 5];
+        let i = 0;
+        while (i < 6) {
+            testArray.push(2);
+            i++
+        }
+        return testArray;
+    },
+    xdoWhile: () => {
+        let testArray = [1, 2, 5, 7, 5];
+        let i = 0;
+        do{
+            testArray.push(3);
+            i++
+        } while (i < 6);
+        return testArray;
+    },
+}
+
+
+//FIRST DAILY
+/*	
+   Write the function that will create this output:
+
+*** the two values are not the same:
+   p1--> a
+   p2--> b
+*** the two values are not the same:
+   p1--> 1
+   p2--> 2
+*** the two values are not the same:
+   p1--> 2
+   p2--> 2
+*/
+
+// Write the function after this comment ---
+
+export const assertEquals = (p1, p2) => {
+    if (p1 === p2) return true
+    console.log(false)
+    /*console.log("*** the two values are not the same” and also log the two parameters as follows:");
+    console.log('“p1-->”' + p1);
+    console.log('“p2-->”' + p2);*/
+    return "the two values are not the same"
+}
+
+
+assertEquals("a", "b");
+assertEquals("a", "a");
+assertEquals(1, 2);
+assertEquals(2, 2);
+assertEquals("2", 2);
+assertEquals("This value", "This value");
+
--- src/javascript/daily.test.js
+++ src/javascript/daily.test.js
@@ -1,13 +1,29 @@
 import {functions, assertEquals, basics, farm} from './daily'
 
 
-test("Check script", () => {
-    expect(assertEquals(1, 1)).toBe(true);
-    expect(assertEquals("a", "b")).toBe("the two values are not the same");
-    expect(assertEquals(1, 2)).toBe("the two values are not the same");
-    expect(assertEquals(2, "2")).toBe("the two values are not the same");
-}); 
+//OCT 16
 
+test('Advanced Loops', () => {
+    expect(farm.xslice()).toEqual(['camel', 'duck', 'elephant']);
+    expect(farm.xsplice()).toEqual(['ant', 'bison', 'camel', 'horse']);
+    expect(farm.xforEach()).toEqual([3, 5, 7, 8,]);
+
+    expect(farm.xmap()).toEqual([ 'ant milk', 'bison milk', 'camel milk', 'horse milk' ]);
+    expect(farm.xreduce()).toEqual(59);
+    expect(farm.xfilter()).toEqual(["bison", "camel", "horse"]);
+    expect(farm.xsort()).toEqual([ 'aardvark', 'ant', 'beaver', 'bison', 'camel', 'cocaroach', 'horse' ]);
+
+})
+
+//OCT 15
+
+test('Prepare Array Work', () => {
+    expect(basics.xfor()).toEqual([3, 4, 7, 9, 7]);
+    expect(basics.xwhile()).toEqual([ 1, 2, 5, 7, 5, 2, 2, 2, 2, 2, 2 ]);
+    expect(basics.xdoWhile()).toEqual([ 1, 2, 5, 7, 5, 3, 3, 3, 3, 3, 3 ]);
+})
+
+//second daily
 /*
  //   Write a function to format an email based on an array.
 */
@@ -22,25 +38,16 @@ test('email builder from an array', () => {
         .toEqual("bill.smith@evolveu.ca");
 });
 
+// First daily
 
-//OCT 15
+test("Check script", () => {
+    expect(assertEquals(1, 1)).toBe(true);
+    expect(assertEquals("a", "b")).toBe("the two values are not the same");
+    expect(assertEquals(1, 2)).toBe("the two values are not the same");
+    expect(assertEquals(2, "2")).toBe("the two values are not the same");
+}); 
 
-test('Prepare Array Work', () => {
-    expect(basics.xfor()).toEqual([3, 4, 7, 9, 7]);
-    expect(basics.xwhile()).toEqual([ 1, 2, 5, 7, 5, 2, 2, 2, 2, 2, 2 ]);
-    expect(basics.xdoWhile()).toEqual([ 1, 2, 5, 7, 5, 3, 3, 3, 3, 3, 3 ]);
-})
 
-//OCT 16
 
-test('Advanced Loops', () => {
-    expect(farm.xslice()).toEqual(['camel', 'duck', 'elephant']);
-    expect(farm.xsplice()).toEqual(['ant', 'bison', 'camel', 'horse']);
-    expect(farm.xforEach()).toEqual([3, 5, 7, 8,]);
 
-    expect(farm.xmap()).toEqual([ 'ant milk', 'bison milk', 'camel milk', 'horse milk' ]);
-    expect(farm.xreduce()).toEqual(59);
-    expect(farm.xfilter()).toEqual(["bison", "camel", "horse"]);
-    expect(farm.xsort()).toEqual([ 'aardvark', 'ant', 'beaver', 'bison', 'camel', 'cocaroach', 'horse' ]);
 
-})
