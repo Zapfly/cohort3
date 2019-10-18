42001d247b3a0e4aadb74885f4cacfcc950cbf1a
Luke <seegerlucas295@gmail.com>
2019-10-18 08:12:43 -0600

    Building Comp 110

---
+++ src/02-dom/scripts/main.js
@@ -0,0 +1,10 @@
+
+
+const showButton = (x) => {
+    console.log(x)        
+}
+
+
+document.getElementById("container").addEventListener("click", addButton);
+document.getElementById("show-button").addEventListener("click", showButton);
+
