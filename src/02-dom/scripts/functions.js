866ac54aec48e0876902784d3f33db1c699c363f
Luke <seegerlucas295@gmail.com>
2019-10-24 17:17:48 -0600

    Added functions.cardCount

--- src/02-dom/scripts/functions.js
+++ src/02-dom/scripts/functions.js
@@ -1,20 +1,22 @@
 const functions = {
-    addCard: (x) => {
-        let cardCount = document.querySelectorAll("div").length;
+    cardCount: 0,
+    addCard: (x, y) => {
         let div = document.createElement("div");
         div.setAttribute("class", "w3-card");
-        div.setAttribute("counter", cardCount);
-        div.textContent = "Card Number" + cardCount;
+        div.setAttribute("counter", y);
+        div.textContent = "Card Number" + y;
         x.appendChild(div);
+        console.log("First count " + y)
+        functions.cardCount = y + 1;
+        console.log("Second count " + y)
                 
     },
     whatCards: (x) => {
         let MTArr = []
         let i = 0;
         let obj = x.children
-        // MTArr.push(obj[1].getAttribute("counter"))
         for (i = 0; i < obj.length; i++) {            
-            MTArr.push(obj[i].getAttribute("counter"))
+            MTArr.push(obj[i].getAttribute("counter"));
         }
         return MTArr
     },
--- src/02-dom/scripts/functions.test.js
+++ src/02-dom/scripts/functions.test.js
@@ -5,7 +5,7 @@ import {functions} from "./functions.js"
 test('Card checking', () => {
     const myDiv = document.createElement("div");
     myDiv.setAttribute("id", "play");
-    functions.addCard(myDiv, 1);
+    functions.addCard(myDiv, functions.cardCount);
 })
 
 test('Card checking', () => {
@@ -13,15 +13,15 @@ test('Card checking', () => {
     group.setAttribute("id", "houseOfCards")
     document.body.appendChild(group)
     
-    functions.addCard(group);
-    functions.addCard(group);
-    functions.addCard(group);
+    functions.addCard(group, functions.cardCount);
+    functions.addCard(group, functions.cardCount);
+    functions.addCard(group, functions.cardCount);
     console.log(document.getElementById("houseOfCards"))
     expect(functions.whatCards(group)).toEqual(["1", "2", "3"])
     
-    functions.addCard(group, 1)    
+    functions.addCard(group, functions.cardCount)    
     expect(functions.whatCards(document.getElementById("houseOfCards"))).toEqual(["1", "2", "3", "4"]);
-    functions.addCard(group, 1)
+    functions.addCard(group, functions.cardCount)
 
 
     
