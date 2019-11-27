aab7fb9429558d01f7222f68753030701cdf0518
Luke <seegerlucas295@gmail.com>
2019-11-26 21:15:02 -0700

    react 02

--- react-02/src/App.js
+++ react-02/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Image from "./components/image"
-import Game from "./components/tick-tack-toe"
+import Game from "./components/Game"
 
 
 
