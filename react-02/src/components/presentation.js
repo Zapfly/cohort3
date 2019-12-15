d574b74d31a30466b1767ff2ca86e46c69cda241
Luke <seegerlucas295@gmail.com>
2019-12-15 07:21:32 -0700

    Testing Slide Functions

--- react-02/src/components/presentation.js
+++ react-02/src/components/presentation.js
@@ -1,22 +1,40 @@
 import React from 'react'
+import greenArrow from '../images/green-arrow.svg'
 
 
 class PresentationPage extends React.Component {
     constructor(props) {
         super(props)
+        this.counter = 0
         this.slide = [
             <Slide1 />,
+            <Slide2 />
         ]
     
         this.state = {
             currentSlide: <Slide1 />             
         }
     }
+
+    previousSlide = () => {
+        this.counter --
+        this.setState({currentSlide: this.slide[this.counter]})
+    }
+
+    nextSlide = () => {
+        this.counter ++
+        this.setState({currentSlide: this.slide[this.counter]})
+    }
     
     render() {
         return (
-            <div>
-                {this.state.currentSlide}                
+            <div className="presentation">
+                <header></header>
+                {this.state.currentSlide}
+                <footer>
+                <img id="previous-slide" src={greenArrow} onClick={this.previousSlide}/>
+                <img id="next-slide" src={greenArrow} onClick={this.nextSlide}/>
+                </footer>                
             </div>
         )
     }
@@ -34,7 +52,26 @@ class Slide1 extends React.Component {
     render() {
         return (
             <div>
-                Why are we here??                
+                <h1>How to Network Effectively</h1>          
+            </div>
+        )
+    }
+    
+}
+
+class Slide2 extends React.Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>It's Really Important</h1>          
             </div>
         )
     }
