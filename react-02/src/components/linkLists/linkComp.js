41daa16c9e7cec0d637fc695a0b833547a033355
Luke <seegerlucas295@gmail.com>
2019-12-23 11:23:38 -0700

    added inputs

--- react-02/src/components/linkLists/linkComp.js
+++ react-02/src/components/linkLists/linkComp.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import  {LinkedList} from './linkPSC'
+import { LinkedList } from './linkPSC'
 
 class CurrentNode extends React.Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             
+
         }
     }
 
-    render () {
+    render() {
         return (
             <div>
                 <p>Subject = {this.props.subject}</p>
@@ -18,38 +18,58 @@ class CurrentNode extends React.Component {
             </div>
         )
     }
-    
+
 }
 
 
 
-export class LinkListComp extends React.Component{
+export class LinkListComp extends React.Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-            list: new LinkedList()            
+            list: new LinkedList(),
+            subInput: "",
+            amountInput: ""
         }
     }
 
-    componentDidMount= () => {
-        this.state.list.insert("Starting Node", 0)
+    componentDidMount = () => {
     }
 
     insert = () => {
-        console.log("button works");
-        
+        console.log(this.state.list.current.subject);
+        this.state.list.insert()
+        console.log(this.state.list.current.forwardNode.subject);
     }
 
+    handleChange = (event) => {
+        const target = event.target;
+        const name = target.name;
+        this.setState({ [name]: event.target.value });
+    }
 
     render() {
-        return <div>
-        <p onClick = {this.insert}>Sample text{/*this.state.list.current.subject*/}</p>
-    </div>
+        this.state.list.insert("Starting Node", 0)
 
-                
+        return (
+            <div>
+                <p >Node List{/*this.state.list.current.subject*/}</p>
+                <CurrentNode 
+                    subject = {this.state.list.current.subject} 
+                    amount = {this.state.list.current.amount}
+                />
+                <input type="text" name="subInput" value={this.state.subInput} onChange={this.handleChange}></input>
+                <input type="number" name="amountInput" value={this.state.amountInput} onChange={this.handleChange}></input>
+                <button onClick={this.insert}>Insert</button>
+                <button>Delete</button>
+            </div>
+        )
+
+
+
 
     }
-    
+
 }
 
