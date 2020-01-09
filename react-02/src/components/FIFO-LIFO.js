a217d765623d26f287bc59a4a7906ef2d592c84e
Luke <seegerlucas295@gmail.com>
2020-01-09 09:48:14 -0700

    feat:ordercheck testing funciton

---
+++ react-02/src/components/FIFO-LIFO.js
@@ -0,0 +1,51 @@
+// FIFO = First In First Out
+// LIFO = Last In First Out
+
+//DnD API
+
+// export const dndApi = {
+//     postData: async (url = '', data = {}) => {
+//         // Default options are marked with *
+//         const response = await fetch(url, {
+//             method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+//             mode: 'cors',       // no-cors, *cors, same-origin
+//             cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+//             credentials: 'same-origin', // include, *same-origin, omit
+//             headers: {
+//                 'Content-Type': 'application/json'
+//                 // 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             redirect: 'follow',         // manual, *follow, error
+//             referrer: 'no-referrer',    // no-referrer, *client
+//             body: JSON.stringify(data)  // body data type must match "Content-Type" header
+//         });
+    
+//         const json = await response.json();    // parses JSON response into native JavaScript objects
+//         json.status = response.status;
+//         json.statusText = response.statusText;
+//         // console.log(json, typeof(json));
+//         return json;
+//       },
+
+//     url: "http://dnd5eapi.co/api/"
+// }
+
+
+//FIFO QUEUE
+
+
+export class FifoLifo {
+    constructor(fifo) {
+        this.counter = 0,
+        this.fifo = fifo,
+        this.contents = {}
+    }
+    
+    add(obj) {
+        this.contents[Number(this.counter)] = obj;
+        obj.order = this.counter
+        this.counter ++;
+    }
+}
+
+
---
+++ react-02/src/components/FIFO-LIFO.test.js
@@ -0,0 +1,53 @@
+import {dndApi, FifoLifo} from './FIFO-LIFO'
+
+// test('accessing api', () => {
+//     const monsters = dndApi.postData(dndApi.url + 'monsters/results')
+//     expect(monsters).toEqual("Aboeleth")
+// })
+
+const orderCheck = (list) => {
+    let result = []
+    let key
+    let i = 0
+
+    for (key in list.contents) {
+        result.push(list.contents[i].order)
+        i++
+    }
+    return result
+}
+
+test('Fifo/Lifo exists', () => {
+    const fifo = new FifoLifo(true)
+    expect(fifo.fifo).toEqual(true)
+    const lifo = new FifoLifo(false)
+    expect(lifo.fifo).toEqual(false)
+})
+
+test('add function', () => {
+    const fifo = new FifoLifo(true)
+    fifo.add({value: "first"})
+    expect(fifo.contents[0].value).toEqual("first")
+    fifo.add({value: "second"})
+    expect(fifo.contents[1].value).toEqual("second")
+    fifo.add({value: "third"})
+    expect(fifo.contents[2].value).toEqual("third")
+})
+ 
+test('Fifo/Lifo ordering', () => {
+    const fifo = new FifoLifo(true)
+
+    fifo.add({value: "first"})
+    expect(orderCheck(fifo)).toEqual([0])
+
+    fifo.add({value: "second"})
+    expect(orderCheck(fifo)).toEqual([0, 1])
+
+    fifo.add({value: "third"})
+    expect(orderCheck(fifo)).toEqual([0, 1, 2])
+
+    fifo.add({value: "fourth"})
+    expect(orderCheck(fifo)).toEqual([0, 1, 2, 3])
+
+})
+
