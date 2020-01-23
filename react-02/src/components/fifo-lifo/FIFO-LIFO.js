e5a25b5d8fdcea922039d9b5760b92b70975a5f5
Luke <seegerlucas295@gmail.com>
2020-01-23 08:23:31 -0700

    feat: delete from Queue

---
+++ react-02/src/components/fifo-lifo/FIFO-LIFO.js
@@ -0,0 +1,38 @@
+import { LinkedList } from "../linkLists/linkPSC";
+
+// FIFO = First In First Out
+// LIFO = Last In First Out
+
+//FIFO QUEUE
+
+
+export class FifoLifo extends LinkedList {
+    constructor(fifo) {
+        super()
+            this.fifo = fifo;
+            this.contents = []
+    }
+    add(obj) {
+        this.contents.push(obj)
+    }
+
+    removeFirst(newArr) {
+        newArr.push(this.contents[0])
+        this.contents.shift()
+    }
+
+    removeLast(newArr) {
+        newArr.push(this.contents[(this.contents.length - 1)])
+        this.contents.pop()
+    }
+
+    removeItem(newArr) {
+        if (this.fifo === true) return this.removeFirst(newArr);
+        else return this.removeLast(newArr)
+    }
+
+}
+
+
+
+
--- react-02/src/components/fifo-lifo/fifo-lifo-Comp.js
+++ react-02/src/components/fifo-lifo/fifo-lifo-Comp.js
@@ -1,20 +1,11 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
-import { FifoLifo } from '../FIFO-LIFO'
+import { FifoLifo } from './FIFO-LIFO'
 
 const Fifo = new FifoLifo(true)
 const Lifo = new FifoLifo(false)
+const DeletedList = new FifoLifo(true)
 
-// const GET = async () => {
-//     const url = "http://dnd5eapi.co/api/monsters/adult-black-dragon"
-//     await fetch(url)
-//     (data =>{return data.json()})
-//     (res =>{console.log(res)})
-// }
-
-// GET()
-
-// dndApi.get()
 
 const Monster = (props) => {
 
@@ -28,85 +19,70 @@ const Monster = (props) => {
     )
 
 }
-
-
-const Stack = () => {
-
-
-    return (
-        <div>
-            <h3>Lifo Stack</h3>
-            <Monster
-                name={/*monsters[0].name*/ "Goblin"}
-                type="Goblinoid Humanoid"
-                cr="1/4"
-            />
-        </div>
-    )
-}
-
-const Deleted = () => {
-
-    return (
-        <div>
-            <h3>Deleted Items</h3>
-            <Monster
-                name={/*monsters[0].name*/ "Goblin"}
-                type="Goblinoid Humanoid"
-                cr="1/4"
-            />
-        </div>
-    )
-}
 const monstersArr = []
 
 const FifoLifoApp = () => {
     const [monsters, setMonsters] = useState([])
+    const [myFifo, setmyFifo] = useState(Fifo)
+    let [fifoCounter, setFifoCounter] = useState(0)
+    const [myLifo, setmyLifo] = useState(Lifo)
+    const [deleted, setDeleted] = useState(DeletedList)
+    const [state, setState] = useState(true)
     let monsterComps;
 
     useEffect(() => {
         async function fetchData() {
             const res = await fetch("https://api.open5e.com/monsters/", {
                 method: 'GET',
-                // mode: 'no-cors',
             });
             let list = await res.json()
             setMonsters(list.results)
-            // console.log(list.results[0].name)
-            // console.log(monstersArr)
         }
         fetchData();
-        // console.log(monsters)
-
     }, []);
+
+    
+
     let key
-    // for (key in monsters) {
-    //     monstersArr.push(
-    //         <Monster
-    //             name={monsters[key].name}
-    //             type={monsters[key].type}
-    //             cr={monsters[key].challenge_rating}
-    //             key={key}
-    //         />
-    //     )
-    // }
     for (key in monsters) {
         monstersArr.push(monsters[key])
     }
+    // console.log(monsters)
 
+    let add = () => {
+        Fifo.add(monstersArr[fifoCounter]);
+        // console.log(Fifo.contents)
+        // setMyFifo(Fifo.contents)
+        setFifoCounter(fifoCounter + 1)
+        // console.log(Fifo.contents)
+    }
 
+    const deleteItem = (list) => {
+        list.removeItem(DeletedList.contents)
+        toggle()
+        
 
-    console.log(monsters)
+    }
 
-
+    const toggle = () => {
+        if (state == true) return setState(false)
+        else return setState(true)
+    }
 
 
     return (
         <div className="fifo-lifo">
             <Queue
+                fifo={myFifo}
+                stack= {myLifo}
+                deleted = {deleteItem}
+                add={add}
             />
-            {/* <Stack />
-            <Deleted /> */}
+            <Stack
+                lifo={myLifo}
+            />
+            <Deleted 
+            deletedList = {DeletedList}/>
         </div>
     )
 }
@@ -117,20 +93,60 @@ const Queue = (props) => {
     // let [myFifo, setMyFifo] = useState(Fifo.contents)
     let [counter, setCounter] = useState(0)
 
-    let add = () => {
-        Fifo.add(monstersArr[counter]);
-        // console.log(Fifo.contents)
-        // setMyFifo(Fifo.contents)
+
+
+    let moveToStack = () => {
+        Fifo.removeItem(Lifo.contents)
         setCounter(counter + 1)
-        console.log(counter)
-        console.log(Fifo.contents)
+        setCounter(counter - 1)
     }
 
-    // useEffect(() => {
 
-    // })
+    const monsterComps =
+        props.fifo.contents.map((item, i) => {
+            // console.log(Fifo.contents)
+            return(
+                <Monster
+                    name={item.name}
+                    type={item.type}
+                    cr={item.challenge_rating}
+                    key={i}
+                />
+            )
+        })
 
-   const monsterComps =  Fifo.contents.map((item, i) => {
+    const deleteThis = () => {
+        props.deleted(props.fifo)
+        console.log(props.fifo.contents)
+    }
+    return (
+        <div >
+            QUEUE:
+
+            <div className="list">
+                {monsterComps}
+            </div>
+            <button onClick={props.add}>Add Monster</button>
+            <button onClick={deleteThis}>Delete</button>
+            {/* <button onClick={moveToStack}>Move to Stack</button> */}
+        </div>
+    )
+}
+
+
+const Stack = (props) => {
+    let [counter, setCounter] = useState(0)
+
+    let add = () => {
+        props.lifo.add(monstersArr[counter]);
+        setCounter(counter + 1)
+        // console.log(counter)
+        // console.log(Fifo.contents)
+    }
+
+
+
+    const monsterComps = props.lifo.contents.map((item, i) => {
         return <Monster
             name={item.name}
             type={item.type}
@@ -140,9 +156,41 @@ const Queue = (props) => {
     })
 
     return (
-        <div>
-            {monsterComps}
-            <button onClick={add}>Add Monster</button>
+        <div >
+            STACK:
+        <div className="list">
+                {monsterComps}
+            </div>
+            <button onClick={add}>Delete</button>
+        </div>
+    )
+}
+
+const Deleted = (props) => {
+
+    let [counter, setCounter] = useState(0)
+
+
+    const monsterComps =
+        props.deletedList.contents.map((item, i) => {
+            // console.log(Fifo.contents)
+            return(
+                <Monster
+                    name={item.name}
+                    type={item.type}
+                    cr={item.challenge_rating}
+                    key={i}
+                />
+            )
+        })
+
+    return (
+        <div >
+            DELETED:
+        <div className="list">
+                {monsterComps}
+            </div>
+            {/* <button onClick={add}>Delete</button> */}
         </div>
     )
 }
