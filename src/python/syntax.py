e6cc6fd2f64ffbbb3e5938f9720fbda46788a8eb
Lucas Seeger <seegerlucas295@gmail.com>
2020-04-21 10:08:09 -0600

    Studying and learning Python

--- src/python/syntax.py
+++ src/python/syntax.py
@@ -28,3 +28,21 @@
 #--------------- Copy ends here --------
 
 #define attributes / variables
+
+
+
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
+
+
+
