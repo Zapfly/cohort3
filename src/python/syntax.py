59f7d822db8155befc0383004c0e5cb7fbe3afd6
Lucas Seeger <seegerlucas295@gmail.com>
2020-04-22 09:34:51 -0600

    Python notes bookmark

--- src/python/syntax.py
+++ src/python/syntax.py
@@ -30,19 +30,24 @@
 #define attributes / variables
 
 
-
-
-
 class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+ 
+  def printname(self):
+    print(self.firstname, self.lastname)
+#-----insert code from the Creating a Parent Class example.-----#
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
 
-  def myfunc(self):
-    print("Hello my name is " + self.name)
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
 
-p1 = Person("John", 36)
-p1.myfunc()
+x = Student("Mike", "Olsen", 2019)
+x.welcome()
 
 
 
